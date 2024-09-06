afda13dc26f36c257e0b6fbb23748c43e9c6e232
Mikhail Arzamastsev <5158679+mikharz@users.noreply.github.com>
2024-09-06 18:01:27 +0300

    Default time zone

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,9 @@
 define('MY_NAMESPACE', 'App\\');
 define('MY_SOURCE_PATH', __DIR__ . '/src/');
 define('MY_SOURCE_EXTENSION', '.php');
+define('MY_TIME_ZONE', 'Europe/Moscow');
+
+date_default_timezone_set(MY_TIME_ZONE);
 
 spl_autoload_register(function ($className) {
     if (str_starts_with($className, MY_NAMESPACE)) {
